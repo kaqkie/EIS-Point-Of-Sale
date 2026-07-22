2145f00410866bf59a8819e7ad028f1d84fcb9e2
Albert Zakulanda <albertzakulanda@gmail.com>
2026-07-22 08:32:25 +0200

    Add system diagnostics features to PointOfSale.App; implement telemetry diagnostics service and related configurations; enhance global exception handling to log telemetry data; update application settings for system diagnostics; modify view models and UI to support diagnostics navigation and health monitoring.

--- src/PointOfSale.Infrastructure/obj/Debug/net8.0/PointOfSale.Infrastructure.AssemblyInfo.cs
+++ src/PointOfSale.Infrastructure/obj/Debug/net8.0/PointOfSale.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © Albert Retail")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd335182e03450d14e5756e50994e533a71bce8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b2e03d53dbff8c479fa9ba61bbd9da734ce6dae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Albert Retail Terminal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PointOfSale.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
